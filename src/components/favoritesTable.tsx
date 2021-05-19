1008d77ceae9cae41821c1d7392cbb7282a5fe45
Max Weisenberger <germanbluemax@gmail.com>
2021-05-19 15:25:45 -0700

    Added a delete button to the fav table and implemented useEffect to fetch new favs on page load
    Co-authored-by: Nachiket Pingle <nachiketpingle@gmail.com>

--- src/components/favoritesTable.tsx
+++ src/components/favoritesTable.tsx
@@ -1,13 +1,16 @@
-import React, { useState, useContext } from "react";
-import { AuthContext } from "./context/Auth.context";
+import React, { useState, useContext, useEffect } from "react";
+import { AuthContext } from "./context/Auth.context"; 
 
 const FavoritesTable = () => {
   const { user, setUser } = useContext(AuthContext);
   const [favorites, setFavorites] = useState({ data: [] });
-  // Write a function that makes table rows with data from PostgreSQL DB
 
-  // Create a function to store the time everytime fetch is called
+  // Write useEffect to call getFavorites on page load
+  useEffect( () => {
+    getFavorites();
+  });
 
+  // Function that creates a timestamp when called
   const lastUpdated = () => {
     let time = new Date();
     return time.toTimeString();
@@ -30,18 +33,33 @@ const FavoritesTable = () => {
     favRowCreator();
   };
 
+
+  const deleteFav = (e) => {
+    // Remove the HTML table row from the DOM
+    document.getElementById(e.target.value).remove();
+
+    // Send a delete request to server to delete the specified favorite from the User's fav table in DB
+    fetch(`/user/${e.target.value}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    })
+  }
+
   const trFromDB = []; // necessary to keep in global scope
   const favRowCreator = () => {
+
     for (const favStations of favorites.data) {
       const tr = (
-        <tr>
+        <tr id={favStations.name}>
           <td>{favStations.station_status}</td>
           <td>{favStations.name}</td>
           <td>{favStations.num_available_bikes}</td>
           <td>{favStations.num_available_ebikes}</td>
           <td>{favStations.stationName}</td>
           <td>{lastUpdated}</td>
+          <td><button id={favStations.name} onClick={deleteFav}>Delete</button></td>
         </tr>
+        // Need to add a delete button to each Table Row (use onClick to invoke a delete func that will send a delete req to server)
       );
       trFromDB.push(tr);
     }
@@ -59,6 +77,7 @@ const FavoritesTable = () => {
           <th>Bikes Available</th>
           <th>eBikes Available</th>
           <th>Last Updated</th>
+          <th>Delete Favorite</th>
         </tr>
         {trFromDB}
       </table>
