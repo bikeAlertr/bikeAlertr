7e5bc5c205232fc57f76acf566a0b474c66a7582
Ty Thanh Doan <tdoan35@gmail.com>
2021-05-19 16:19:58 -0700

    built out backend

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,35 +1,41 @@
 const express = require('express');
-const { stat } = require('fs');
-const { nextTick } = require('process');
+// const { stat } = require('fs');
+// const { nextTick } = require('process');
 
 const stationController = require('../controllers/stationController');
 const userController = require('../controllers/userController');
+const favoritesController = require('../controllers/favoritesController');
+const alertsController = require('../controllers/alertsController')
 
 const router = express.Router();
 
-
 // ----------------- STATION CONTROLLER ----------------------------------------------------------
 
-router.get('/addStationInfo', 
-  stationController.addStationInfo, (err, res, req) => {
-    res.status(200).json('Successfully got the station info!');
-  }
-);
+// INITIAL ADD ALL STATIONS TO DATABASE
+// router.get('/addStationInfo', 
+//   stationController.addStationInfo, (err, res, req) => {
+//     res.status(200).json('Successfully got the station info!');
+//   }
+// );
 
-router.get('/test',
-  (err, res, req) => {
-    console.log('asyncronous!');
-    res.status(200).json('Successfully got the asyncronous call!');
-  }
-);
+// router.get('/test',
+//   (err, res, req) => {
+//     console.log('asyncronous!');
+//     res.status(200).json('Successfully got the asyncronous call!');
+//   }
+// );
 
-router.get('/updateStation', stationController.updateStations, 
+// DYNAMICALLY UPDATE THE STATIONS TABLE EVERY MINUTE
+router.get('/updateStation', 
+  stationController.updateStations, 
   (err, res, req) => {
     res.status(200).json('Successfully updated the SQL Table!')
   }
 );
 
-router.get('/refreshStation', stationController.updateStations, 
+// REFRESH DATA IN THE DATABASE
+router.get('/refreshStation', 
+  stationController.updateStations, 
   (err, res, req) => {
     res.status(200).json('Successfully refreshed the SQL Table!')
   }
@@ -37,29 +43,100 @@ router.get('/refreshStation', stationController.updateStations,
 
 // -----------------------------------------------------------------------------------------------
 
-// ----------------------------------- USER CONTROLLERS ----------------------------------------------//
+// ----------------------------------- USER CONTROLLER -------------------------------------------
 
-// Add new user to database (signup)
+// SIGNUP NEW USER TO DATABASE 
 router.post('/signup', 
   userController.signUp,
+  userController.setSSIDCookie,
   (err, res, req) => {
-    res.status(200).send('Successfully signed up new user');
+    res.status(200).send(res.locals);
   }
 );
 
-router.post('/login', userController.login,
-(err, res, req) => {
-  res.status(200).send(res.locals);
-}
+// VERIFY USER INFO FROM DB TO LOGIN
+router.post('/login', 
+  userController.login,
+  userController.setSSIDCookie,
+  (err, res, req) => {
+    res.status(200).send(res.locals);
+  }
 );
 
-// Verify user info from db to login
+// VERIFY IF USER IS LOGGED IN
+router.get('/isLoggedIn', 
+  userController.isLoggedIn, 
+  (err, res, req) => {
+    res.status(200).json(res.locals);
+});
 
-// Verify if user is logged in
+// DELETE COOKIES WHEN USER SIGNOUT
+router.get('/signout', 
+  (err, req, res) => {
+    res.clearCookie('ssid');
+    res.send('cookie ssid cleared');
+})
 
-// Delete cookies when user logsout (signout)
+// -----------------------------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------------
+// ----------------------------------- ALERTS CONTROLLER -----------------------------------------
+
+// CREATE NEW ALERTS 
+router.post('/addAlert', 
+  alertsController.addAlert, 
+  (err, res, req) => {
+    res.status(200).send(res.locals);
+});
+
+// READ (GET) ALERTS INFO
+router.get('/getAlerts',
+  alertsController.getAlerts,
+  (err, res, req) => {
+    res.status(200).send(res.locals);
+});
+
+// DELETE ALERTS FROM USERS DB
+router.delete('/deleteAlert',
+  alertsController.deleteAlert,
+  (err, res, req) => {
+    res.status(200).send(res.locals);
+});
+
+// user's alerts: Array<string>
+// alert = "STATION #72 IS LOW CAPACITY (<25%)" - target MVP
+// alert = "STATION #86 IS OUT OF EBIKES (0/4)"
+// alert = "STATION #23 HAS LOW DOCK CAPACITY (<25%)"
+// alert = "STATION #62 IS OUT OF SERVICE"
+
+// -----------------------------------------------------------------------------------------------
+
+// ---------------------------- FAVORITE STATIONS CONTROLLER -------------------------------------
+
+// CREATE NEW FAVORITE STATIONS 
+router.post('/addFavStation', 
+  favoritesController.addFavStation, 
+  (err, res, req) => {
+    res.status(200).send(res.locals);
+  }
+);
+
+// READ (GET) FAVORITE STATIONS INFO
+router.get('/getFavStations', 
+  favoritesController.getFavStations, 
+  (err, res, req) => {
+  res.status(200).send(res.locals);
+  }
+);
+
+// DELETE FAVORITE STATIONS FROM USERS DB
+router.delete('/deleteFavStations', 
+  favoritesController.deleteFavStations, 
+  (err, res, req) => {
+  res.status(200).send(res.locals);
+  }
+);
+
+// -----------------------------------------------------------------------------------------------
 
 
 module.exports = router;
