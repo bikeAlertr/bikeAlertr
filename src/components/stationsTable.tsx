6c566ddbbefefc94a8006d5e20e0cff885ac6452
Max Weisenberger <germanbluemax@gmail.com>
2021-05-19 22:46:23 -0700

    Working on rendering table rows from stationstable component
    Co-authored-by: Nachiket Pingle <nachiketpingle@gmail.com>,
    Co-authored-by: Max Weisenberger <germanbluemax@gmail.com>,
    Co-authored-by: Ty Doan <tdoan35@gmail.com>,
    Co-authored-by: Sebastian Damazo <ernesto01@ucsb.edu>

--- src/components/stationsTable.tsx
+++ src/components/stationsTable.tsx
@@ -2,17 +2,22 @@ import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "./context/Auth.context"; 
 
 const StationsTable = () => {
-  const { user, setUser } = useContext(AuthContext);
-  const [stations, setStations] = useState([]);
-
+  // const { user, setUser } = useContext(AuthContext);
+  const [stations, setStations] = useState({});
+  const [test, setTest] = useState(0)
   // Write useEffect to call getFavorites on page load
   useEffect( () => {
     // console.log('this is user in stationstable.tsx', user);
-    getStations();
-    // console.log('stations in useeffect', stations)
+    console.log('stations in useeffect', stations)
     stationRowCreator(stations);
-  }, []);
+  }, [stations]);
 
+
+  useEffect( () => {
+    // console.log('this is user in stationstable.tsx', user);
+    console.log('stations in useeffect', stations)
+    getStations()
+  }, []);
   // Function that creates a timestamp when called
   const lastUpdated = () => {
     let time = new Date();
@@ -29,8 +34,8 @@ const StationsTable = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log('res.locals after getstations is', data );
-        setStations(data);
-        // console.log("stations", stations);
+        setStations({...stations, data})
+        console.log("stations", stations);
       });
 
     lastUpdated(); // to store the time of this fetch req
@@ -48,24 +53,34 @@ const StationsTable = () => {
   }
 
   const trFromDB = []; // necessary to keep in global scope
-  const stationRowCreator = (stations) => {
-    console.log('this is stations in row creator', stations)
-    for (let i = 0; i < stations.length; i++) {
-      const tr = (
-        <tr id={stations[i].name}>
-          <td>{stations[i].station_status}</td>
-          <td>{stations[i].name}</td>
-          <td>{stations[i].num_available_bikes}</td>
-          <td>{stations[i].num_available_ebikes}</td>
-          <td>{stations[i].stationName}</td>
-          <td>{lastUpdated}</td>
-          <td><button id={stations[i].name} onClick={favStation}>Favorite</button></td>
-        </tr>
-      );
-      trFromDB.push(tr);
+  const stationRowCreator = async (stations) => {
+    console.log('this is stations in row creator', stations.data)
+    if (Array.isArray(stations.data)) {
+      console.log(stations.data.length)
+      for (let i = 0; i < stations.data.length; i++) {
+        trFromDB.push(
+          <tr id={stations.data[i].station_id}>
+            <td>{stations.data[i].station_status}</td>
+            <td>{stations.data[i].name}</td>
+            <td>{stations.data[i].num_available_bikes}</td>
+            <td>{stations.data[i].num_available_ebikes}</td>
+            <td>{stations.data[i].stationName}</td>
+            <td>{lastUpdated()}</td>
+            <td><button id={stations.data[i].station_id} onClick={favStation}>Favorite</button></td>
+          </tr>
+        );
+        // trFromDB.push(tr);
+       
+      }
+      console.log('this is tr from db', trFromDB);
+      return trFromDB;      
     }
-    return trFromDB;
   };
+
+  useEffect( () => {
+    // console.log('this is user in stationstable.tsx', user);
+    console.log('RENDERED AGAIN')
+  }, [stationRowCreator]);
 
   return (
     <div>
@@ -78,6 +93,7 @@ const StationsTable = () => {
           <th>Last Updated</th>
           <th>Add Favorite</th>
         </tr>
+        {console.log('in return', trFromDB)}
         {trFromDB}
       </table>
     </div>
