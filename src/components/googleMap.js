e0bf38b42e1c3b10ccd2422b2b9f0467db80613f
Max Weisenberger <germanbluemax@gmail.com>
2021-05-20 12:40:37 -0700

    Added google maps component, renders markers from DB data
    Co-authored-by: Nachiket Pingle <nachiketpingle@gmail.com>,
    Co-authored-by: Max Weisenberger <germanbluemax@gmail.com>,
    Co-authored-by: Ty Doan <tdoan35@gmail.com>,
    Co-authored-by: Sebastian Damazo <ernesto01@ucsb.edu>

---
+++ src/components/googleMap.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 40.797873,
+  lng: -73.919965
+};
+
+function EasyGoogleMap() {
+  const [stations, setStations] = useState({});
+  
+  useEffect( () => {
+    getStations()
+  }, []);
+
+  const getStations = () => {
+    // Retrieve the required data
+    fetch(`/api/stations`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('res.locals after getstations is', data );
+        setStations({...stations, data})
+        console.log("stations", stations);
+      });
+    }
+
+    const genMarkers = []
+    const makeMarkers = (stations) => {
+      console.log('in makemarkers stations', stations.data)
+      if (Array.isArray(stations.data)) {
+        for (let el of stations.data) {
+          const location = {
+            lat: +el.latitude,
+            lng: +el.longitude
+          };
+          console.log('this is latittuuuude', location)
+          genMarkers.push(<Marker key={el.station_id} position={location} />)
+        }
+      }
+    }
+
+    makeMarkers(stations);
+
+  return (
+    <LoadScript
+      googleMapsApiKey= 'ADD API KEY HERE';
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+        {genMarkers}
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(EasyGoogleMap);
